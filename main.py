509d6273412c18f0259d56d4b61bc96dfb7aa86d
Yurtema <yurtema@gmail.com>
2022-03-19 11:07:32 +0300

    Added secnd layer of neurons

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5,  2.7, 3.3, -0.8]]
+
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
+
 biases = [2, 3, 0.5]
 
-output = np.dot(weights, inputs) + biases
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+output = np.dot(inputs, np.array(weights).T) + biases
+output2 = np.dot(output, np.array(weights2).T) + biases2
 
 print(output)
+print(output2)
