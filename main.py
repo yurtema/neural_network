9702a07fdfae732bab9656a30398449a90ec8bbd
Yurtema <yurtema@gmail.com>
2022-03-22 11:24:08 +0300

    created class Layer

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5,  2.7, 3.3, -0.8]]
+np.random.seed(0)
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-biases = [2, 3, 0.5]
 
-weights2 = [[0.1, -0.14, 0.5],
-            [-0.5, 0.12, -0.33],
-            [-0.44, 0.73, -0.13]]
+class Layer():
+	def __init__(self, n_inputs, n_neurons):
+		self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+		self.biases = np.zeros(n_neurons)
+	
+	def forward(self):
+		pass
 
-biases2 = [-1, 2, -0.5]
-
-output = np.dot(inputs, np.array(weights).T) + biases
-output2 = np.dot(output, np.array(weights2).T) + biases2
-
-print(output)
-print(output2)
