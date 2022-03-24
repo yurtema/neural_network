eaaa661b7e562bd799b36af4686bbfc261977cf8
Yurtema <yurtema@gmail.com>
2022-03-24 22:49:10 +0300

    ReLu activation fuction added

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
-
+nnfs.init()
+'''
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
+'''
+X, y = spiral_data(100, 3)
 
-
-class Layer:
+class Layer_Dense:
 	
 	def __init__(self, n_inputs, n_neurons):
 		self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
 		self.biases = np.zeros(n_neurons)
 	
-	def process(self, inputs):
+	def forward(self, inputs):
 		self.output = np.dot(inputs, self.weights) + self.biases
 
 
+class Activation_ReLu:
+	def forward(self, inputs):
+		self.output = np.maximum(0, inputs)
 
 
-layer1 = Layer(4, 5)
-layer2 = Layer(5, 2)
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLu()
 
-layer1.process(X)
-# print(layer1.output)
-layer2.process(layer1.output)
-print(layer2.output)
+layer1.forward(X)
+activation1.forward(layer1.output)
+print(activation1.output)
