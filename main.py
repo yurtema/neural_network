ad650dd74f98a4a820fa93068331b6386f360031
Yurtema <yurtema@gmail.com>
2022-03-26 10:10:15 +0300

    added softmax actication function

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ import nnfs
 from nnfs.datasets import spiral_data
 
 nnfs.init()
-'''
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-'''
-X, y = spiral_data(100, 3)
 
 class Layer_Dense:
 	
@@ -19,15 +13,30 @@ class Layer_Dense:
 	def forward(self, inputs):
 		self.output = np.dot(inputs, self.weights) + self.biases
 
-
 class Activation_ReLu:
 	def forward(self, inputs):
 		self.output = np.maximum(0, inputs)
+		
+class Activation_Softmax:
+	def forward(self, inputs):
+		exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+		probabilities = exp_values/np.sum(exp_values, axis=1, keepdims=True)
+		self.output = probabilities
 
 
-layer1 = Layer_Dense(2, 5)
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(n_inputs=2, n_neurons=3)
 activation1 = Activation_ReLu()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
